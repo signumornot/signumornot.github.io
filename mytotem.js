9fa1aa8ec1bfe076e3b761fb806d9b289c835ef3
signumornot <124530130+signumornot@users.noreply.github.com>
2023-02-06 15:19:03 +0400

    Update mytotem.js

--- mytotem.js
+++ mytotem.js
@@ -1,15 +1,16 @@
 function mytotem() 
     { 
     var beast = ['Барс', 'Сова'];
-var beastrand = Math.floor(Math.random()*beast.length);
-var beastvalue = beast[beastrand];
+const beastrand = Math.floor(Math.random()*beast.length);
+const beastvalue = beast[beastrand];
 
 var char = ['« <b>Независимость</b> »', '« <b>Хитрость</b> »'];
 var charrand = Math.floor(Math.random()*char.length);
 var charvalue = char[charrand];
       
 var pic = ['<img src="http://d.zaix.ru/6SXg.png" border="0">', '<img src="http://d.zaix.ru/htFZ.png" border="0">'];
-var picvalue = pic[beastrand];
+var picrand = beastrand;
+var picvalue = pic[picrand];
 
 document.getElementById("beast").innerHTML = beastvalue;
 document.getElementById("char").innerHTML = charvalue;
